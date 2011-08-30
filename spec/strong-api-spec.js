92170b2f708dc3417e3aa2ab9026bd93b66949f9
Tim Shadel <tim@shadelsoftware.com>
2011-08-30 17:44:43 -0600

    Simplify the API loading; 1st draft of backend
    Next up is string interpolation.

--- spec/strong-api-spec.js
+++ spec/strong-api-spec.js
@@ -1,41 +1,48 @@
-describe('strong-api', function(){
-  var StrongAPI = require('../lib/strong-api');
-  var api;
+describe('strong', function(){
+  var strong = require('../lib/strong');
   
   beforeEach(function () {
-    api = new StrongAPI();
+    strong.init();
   });
 
   it('should translate in the default default locale', function() {
-    expect(api.translate('everything')).toEqual('I am a translated string for locale: en');
+    strong.back.put('en.everything', 'I am a translated string for locale: en');
+    expect(strong.translate('everything')).toEqual('I am a translated string for locale: en');
   });
 
   it('should translate in the recently set default locale', function() {
-    api.default_locale = 'de';
-    expect(api.translate('everything')).toEqual('I am a translated string for locale: de');
+    strong.back.put('de.everything', 'I am a translated string for locale: de');
+    strong.default_locale = 'de';
+    expect(strong.translate('everything')).toEqual('I am a translated string for locale: de');
   });
 
   it('should translate in the current locale', function() {
-    api.default_locale = 'de';
-    api.locale = 'en';
-    expect(api.translate('everything')).toEqual('I am a translated string for locale: en');
+    strong.back.put('en.everything', 'I am a translated string for locale: en');
+    strong.default_locale = 'de';
+    strong.locale = 'en';
+    expect(strong.translate('everything')).toEqual('I am a translated string for locale: en');
   });
 
   it('should translate in the option-specified locale', function() {
-    api.default_locale = 'de';
-    api.locale = 'en';
-    expect(api.translate('everything', { locale: 'es' })).toEqual('I am a translated string for locale: es');
+    strong.back.put('es.everything', 'I am a translated string for locale: es');
+    strong.default_locale = 'de';
+    strong.locale = 'en';
+    expect(strong.translate('everything', { locale: 'es' })).toEqual('I am a translated string for locale: es');
   });
 
   // Interpolation
-  it('should use named arguments as substitutions', function() {
-    // Setup the test case, assuming you have this string
-    var fake_back = { 'en': { '.hello': 'Hello, %(name)' } };
-
-    // Make sure you can toss variables at it
-    var user = {'name': 'Johnny'};
-    var result = api.translate( '.hello', user );
-    expect(result).toEqual( 'Hello, Johnny' );
-  });
+  // it('should use named arguments as substitutions', function() {
+  //   strong.back.put('en.hello', 'Hello, {name}');
+  //   // Make sure you can toss variables at it
+  //   var user = { name: 'Johnny' };
+  //   expect(strong.translate( 'hello', user )).toEqual( 'Hello, Johnny' );
+  // });
+  // 
+  // it('should navigate the object to fill substitution arguments', function() {
+  //   strong.back.put('en.hello', 'Hello, {name.first}');
+  //   // Make sure you can use dot notation
+  //   var user = { name: { first: 'Johnny', last: 'Smith' } };
+  //   expect(strong.translate( 'hello', user )).toEqual( 'Hello, Johnny' );
+  // });
 
 });
