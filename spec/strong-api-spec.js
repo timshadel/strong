8f9080332ffc094f3c40e809e3906d094ef68624
Logan Allred <redbugz@gmail.com>
2012-07-23 18:10:20 -0600

    Add support for lists in the locale property
    Since the accept-languages header can return a list of locales in preferred order, strong should be able to resolve the best locale from the list. This will allow the locale property to be set either as a string or an array (strings are converted to an array internally) and the locales will be searched in order for a matching key.

--- spec/strong-api-spec.js
+++ spec/strong-api-spec.js
@@ -12,7 +12,14 @@ describe('strong', function(){
 
   it('should translate in the recently set default locale', function() {
     strong.back.putAtPath('de.everything', 'I am a translated string for locale: de');
-    strong.default_locale = 'de';
+    strong.default_locale = 'DE';
+    expect(strong.translate('everything')).toEqual('I am a translated string for locale: de');
+  });
+
+  it('should translate in the default locale if not found in locale', function() {
+    strong.back.putAtPath('de.everything', 'I am a translated string for locale: de');
+    strong.default_locale = 'DE';
+    strong.locale = [ 'es', 'zh' ];
     expect(strong.translate('everything')).toEqual('I am a translated string for locale: de');
   });
 
@@ -23,11 +30,41 @@ describe('strong', function(){
     expect(strong.translate('everything')).toEqual('I am a translated string for locale: en');
   });
 
+  it('should translate in the current locale list', function() {
+    strong.back.putAtPath('en.everything', 'I am a translated string for locale: en');
+    strong.default_locale = 'de';
+    strong.locale = ['es', 'en'];
+    expect(strong.translate('everything')).toEqual('I am a translated string for locale: en');
+  });
+
+  it('should translate in the current locale list', function() {
+    strong.back.putAtPath('en.everything', 'I am a translated string for locale: en');
+    strong.back.putAtPath('de.everything', 'I am a translated string for locale: de');
+    strong.back.putAtPath('es.everything', 'I am a translated string for locale: es');
+    strong.default_locale = 'de';
+    strong.locale = ['es', 'en'];
+    expect(strong.translate('everything')).toEqual('I am a translated string for locale: es');
+  });
+
+  it('should translate in the current locale list', function() {
+    strong.back.putAtPath('en.everything', 'I am a translated string for locale: en');
+    strong.default_locale = 'de';
+    strong.locale = ['es', 'en'];
+    expect(strong.translate('everything')).toEqual('I am a translated string for locale: en');
+  });
+
   it('should translate in the option-specified locale', function() {
     strong.back.putAtPath('es.everything', 'I am a translated string for locale: es');
     strong.default_locale = 'de';
     strong.locale = 'en';
     expect(strong.translate('everything', { locale: 'es' })).toEqual('I am a translated string for locale: es');
+  });
+
+  it('should translate in the option-specified locale list', function() {
+    strong.back.putAtPath('es.everything', 'I am a translated string for locale: es');
+    strong.default_locale = 'de';
+    strong.locale = 'en';
+    expect(strong.translate('everything', { locale: [ 'pt', 'es' ] })).toEqual('I am a translated string for locale: es');
   });
 
   // Interpolation
@@ -58,6 +95,14 @@ describe('strong', function(){
   it('should raise a TranslationNotFound error when the key isn\'t found.', function() {
     strong.back.putAtPath('zh.everything', 'I am a translated string for locale: zh');
     expect(function(){ strong.translate('everything') }).toThrow("TranslationNotFound: Could not find key 'everything'.  Attempted: 'en.everything'");
+  });
+
+  it('should raise a TranslationNotFound error when the key isn\'t found in multiple locales.', function() {
+    strong.back.putAtPath('zh.everything', 'I am a translated string for locale: zh');
+    strong.default_locale = 'DE';
+    // duplicate locale (en-us) should be ignored
+    strong.locale = [ 'pt-PT', 'pt', 'en-US', 'en', 'es-ES', 'ES', 'en-gb', 'en-us', 'pt-br' ];
+    expect(function(){ strong.translate('everything') }).toThrow("TranslationNotFound: Could not find key 'everything'.  Attempted: 'pt-pt.everything', 'pt.everything', 'en-us.everything', 'en.everything', 'es-es.everything', 'es.everything', 'en-gb.everything', 'pt-br.everything', 'de.everything'");
   });
 
 
