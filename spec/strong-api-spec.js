a35a98d96455f0fabb99a3c2df4f8d5de8924e54
Tim Shadel <tim@shadelsoftware.com>
2013-02-12 14:03:59 -0700

    Move to Mocha & Should

--- spec/strong-api-spec.js
+++ spec/strong-api-spec.js
@@ -1,6 +1,9 @@
+var sinon = require('sinon')
+  , should = require('should');
+
 describe('strong', function(){
-  var strong = require('../lib/strong');
-  var res = {};
+  var strong = require('../lib/strong')
+    , res = {};
 
   beforeEach(function () {
     strong.init();
@@ -8,41 +11,41 @@ describe('strong', function(){
 
   it('should translate in the default locale', function() {
     strong.back.putAtPath('en.everything', 'I am a translated string for locale: en');
-    expect(strong.translate('everything')).toEqual('I am a translated string for locale: en');
+    strong.translate('everything').should.equal('I am a translated string for locale: en');
   });
 
   it('should translate in the recently set default locale', function() {
     strong.back.putAtPath('de.everything', 'I am a translated string for locale: de');
     strong.default_locale = 'DE';
-    expect(strong.translate('everything')).toEqual('I am a translated string for locale: de');
+    strong.translate('everything').should.equal('I am a translated string for locale: de');
   });
 
   it('should translate in IETF default locale', function() {
     strong.back.putAtPath('pt.everything', 'I am a translated string for locale: pt');
     strong.back.putAtPath('pt-br.everything', 'I am a translated string for locale: pt-br');
     strong.default_locale = 'pt-BR';
-    expect(strong.translate('everything')).toEqual('I am a translated string for locale: pt-br');
+    strong.translate('everything').should.equal('I am a translated string for locale: pt-br');
   });
 
   it('should translate in the default locale if not found in locale', function() {
     strong.back.putAtPath('de.everything', 'I am a translated string for locale: de');
     strong.default_locale = 'DE';
     strong.locale = [ 'es', 'zh' ];
-    expect(strong.translate('everything')).toEqual('I am a translated string for locale: de');
+    strong.translate('everything').should.equal('I am a translated string for locale: de');
   });
 
   it('should translate in the current locale', function() {
     strong.back.putAtPath('en.everything', 'I am a translated string for locale: en');
     strong.default_locale = 'de';
     strong.locale = 'en';
-    expect(strong.translate('everything')).toEqual('I am a translated string for locale: en');
+    strong.translate('everything').should.equal('I am a translated string for locale: en');
   });
 
   it('should translate in the current IETF locale', function() {
     strong.back.putAtPath('es-mx.everything', 'I am a translated string for locale: es-mx');
     strong.default_locale = 'de';
     strong.locale = 'es-MX';
-    expect(strong.translate('everything')).toEqual('I am a translated string for locale: es-mx');
+    strong.translate('everything').should.equal('I am a translated string for locale: es-mx');
   });
 
   it('should translate in the current IETF locale with fallback', function() {
@@ -50,14 +53,14 @@ describe('strong', function(){
     strong.back.putAtPath('es.everything', 'I am a translated string for locale: es');
     strong.default_locale = 'de';
     strong.locale = 'es-MX';
-    expect(strong.translate('everything')).toEqual('I am a translated string for locale: es');
+    strong.translate('everything').should.equal('I am a translated string for locale: es');
   });
 
   it('should translate in the current locale list', function() {
     strong.back.putAtPath('en.everything', 'I am a translated string for locale: en');
     strong.default_locale = 'de';
     strong.locale = ['es', 'en'];
-    expect(strong.translate('everything')).toEqual('I am a translated string for locale: en');
+    strong.translate('everything').should.equal('I am a translated string for locale: en');
   });
 
   it('should translate in the current locale list', function() {
@@ -66,28 +69,28 @@ describe('strong', function(){
     strong.back.putAtPath('es.everything', 'I am a translated string for locale: es');
     strong.default_locale = 'de';
     strong.locale = ['es', 'en'];
-    expect(strong.translate('everything')).toEqual('I am a translated string for locale: es');
+    strong.translate('everything').should.equal('I am a translated string for locale: es');
   });
 
   it('should translate in the current locale list', function() {
     strong.back.putAtPath('en.everything', 'I am a translated string for locale: en');
     strong.default_locale = 'de';
     strong.locale = ['es', 'en'];
-    expect(strong.translate('everything')).toEqual('I am a translated string for locale: en');
+    strong.translate('everything').should.equal('I am a translated string for locale: en');
   });
 
   it('should translate in the option-specified locale', function() {
     strong.back.putAtPath('es.everything', 'I am a translated string for locale: es');
     strong.default_locale = 'de';
     strong.locale = 'en';
-    expect(strong.translate('everything', { locale: 'es' })).toEqual('I am a translated string for locale: es');
+    strong.translate('everything', { locale: 'es' }).should.equal('I am a translated string for locale: es');
   });
 
   it('should translate in the option-specified locale list', function() {
     strong.back.putAtPath('es.everything', 'I am a translated string for locale: es');
     strong.default_locale = 'de';
     strong.locale = 'en';
-    expect(strong.translate('everything', { locale: [ 'pt', 'es' ] })).toEqual('I am a translated string for locale: es');
+    strong.translate('everything', { locale: [ 'pt', 'es' ] }).should.equal('I am a translated string for locale: es');
   });
 
   // Interpolation
@@ -95,29 +98,29 @@ describe('strong', function(){
     strong.back.putAtPath('en.hello', 'Hello, %{name}');
     // Make sure you can toss variables at it
     var user = { name: 'Johnny' };
-    expect(strong.translate( 'hello', user )).toEqual( 'Hello, Johnny' );
+    strong.translate( 'hello', user ).should.equal( 'Hello, Johnny' );
   });
   
   it('should navigate the object to fill substitution arguments', function() {
     strong.back.putAtPath('en.hello', 'Hello, %{name.first}');
     // Make sure you can use dot notation
     var user = { name: { first: 'Johnny', last: 'Smith' } };
-    expect(strong.translate( 'hello', user )).toEqual( 'Hello, Johnny' );
+    strong.translate( 'hello', user ).should.equal( 'Hello, Johnny' );
   });
 
   // Pluralization
   it('should select an appropriate pluralization option', function() {
     strong.back.putAtPath('en.message_count', { one: '1 message', other: '%{count} messages' } );
   
-    expect(strong.translate( 'message_count', { count: 0 } )).toEqual( '0 messages' );
-    expect(strong.translate( 'message_count', { count: 1 } )).toEqual( '1 message' );
-    expect(strong.translate( 'message_count', { count: 2 } )).toEqual( '2 messages' );
+    strong.translate( 'message_count', { count: 0 } ).should.equal( '0 messages' );
+    strong.translate( 'message_count', { count: 1 } ).should.equal( '1 message' );
+    strong.translate( 'message_count', { count: 2 } ).should.equal( '2 messages' );
   });
 
   // Not found
   it('should raise a TranslationNotFound error when the key isn\'t found.', function() {
     strong.back.putAtPath('zh.everything', 'I am a translated string for locale: zh');
-    expect(function(){ strong.translate('everything') }).toThrow("TranslationNotFound: Could not find key 'everything'.  Attempted: 'en.everything'");
+    (function(){ strong.translate('everything'); }).should.throw("TranslationNotFound: Could not find key 'everything'.  Attempted: 'en.everything'");
   });
 
   it('should raise a TranslationNotFound error when the key isn\'t found in multiple locales.', function() {
@@ -125,7 +128,7 @@ describe('strong', function(){
     strong.default_locale = 'DE';
     // duplicate locale (en-us) should be ignored
     strong.locale = [ 'pt-PT', 'pt', 'en-US', 'en', 'es-ES', 'ES', 'en-gb', 'en-us', 'pt-br' ];
-    expect(function(){ strong.translate('everything') }).toThrow("TranslationNotFound: Could not find key 'everything'.  Attempted: 'pt-pt.everything', 'pt.everything', 'en-us.everything', 'en.everything', 'es-es.everything', 'es.everything', 'en-gb.everything', 'pt-br.everything', 'de.everything'");
+    (function(){ strong.translate('everything'); }).should.throw("TranslationNotFound: Could not find key 'everything'.  Attempted: 'pt-pt.everything', 'pt.everything', 'en-us.everything', 'en.everything', 'es-es.everything', 'es.everything', 'en-gb.everything', 'pt-br.everything', 'de.everything'");
   });
 
   // localeHelper
@@ -134,27 +137,27 @@ describe('strong', function(){
       headers: {
         "accept-language": "pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.6,es-ES;q=0.5,es;q=0.4,en-gb;q=0.3,pt-br;q=0.1"
       }
-    }
+    };
     var locale = strong.localeHelper(req, res);
-    expect(locale).toEqual([ 'pt-PT', 'pt', 'en-US', 'en', 'es-ES', 'es', 'en-gb', 'pt-br' ]);
+    locale.should.eql([ 'pt-PT', 'pt', 'en-US', 'en', 'es-ES', 'es', 'en-gb', 'pt-br' ]);
   });
 
   it('should work with missing accept-language header.', function() {
     var req = {
       headers: {}
-    }
+    };
     var locale = strong.localeHelper(req, res);
 
-    expect(locale).toBeUndefined();
+    should.not.exist(locale);
   });
 
   it('should work with Express 3 req.acceptedLanguages.', function() {
     var req = {
       acceptedLanguages: [ 'pt-PT', 'pt', 'en-US', 'en', 'es-ES', 'ES', 'en-gb', 'pt-br' ]
-    }
+    };
     var locale = strong.localeHelper(req, res);
 
-    expect(locale).toEqual(req.acceptedLanguages);
+    locale.should.equal(req.acceptedLanguages);
   });
 });
 
@@ -169,12 +172,12 @@ describe('strong.decorator', function () {
       compile: function () {
         return function (mockData) {
           return mockData;
-        }
+        };
       }
-    }
+    };
     var compileFunction = strong.decorator(mockEngine, "mockAppName").compile({}, {filename: "testfile"});
     var template = compileFunction(mockData);
-    translateMock = jasmine.createSpy("translateMock");
+    translateMock = sinon.spy();
     originalTranslate = strong.translate;
     strong.translate = translateMock;
   });
@@ -184,24 +187,24 @@ describe('strong.decorator', function () {
   });
 
   it('should create a t() and i18n() function.', function () {
-    expect(mockData.t).toEqual(jasmine.any(Function));
-    expect(mockData.i18n).toEqual(jasmine.any(Function));
-    expect(mockData.i18n).toEqual(mockData.t);
+    mockData.t.should.be.a('function');
+    mockData.i18n.should.be.a('function');
+    mockData.i18n.should.equal(mockData.t);
   });
 
   it('should call translate with key and view_path.', function () {
     mockData.t.apply({}, ['everything']);
-    expect(translateMock).toHaveBeenCalledWith("everything", { view_path: [ 'mockAppName', 'testfile' ] });
+    translateMock.calledWith("everything", { view_path: [ 'mockAppName', 'testfile' ] }).should.be.ok;
   });
 
   it('should call translate with res.locals.locale if it exists.', function () {
     mockData.t.apply({locale:['es','zh']}, ['everything']);
-    expect(translateMock).toHaveBeenCalledWith("everything", { locale: ['es', 'zh'], view_path: [ 'mockAppName', 'testfile' ] });
+    translateMock.calledWith("everything", { locale: ['es', 'zh'], view_path: [ 'mockAppName', 'testfile' ] }).should.be.ok;
   });
 
   it('should override res.locals.locale with options passed to t().', function () {
     mockData.t.apply({locals:{locale:['es','zh']}}, ['everything', {locale:['pt-BR', 'en']}]);
-    expect(translateMock).toHaveBeenCalledWith("everything", { locale: ['pt-BR', 'en'], view_path: [ 'mockAppName', 'testfile' ] });
+    translateMock.calledWith("everything", { locale: ['pt-BR', 'en'], view_path: [ 'mockAppName', 'testfile' ] }).should.be.ok;
   });
 
 });
